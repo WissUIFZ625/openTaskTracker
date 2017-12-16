dffbcb9388c8b93ca0d76a74cf252968df31fbf6
Ivo <ivo@eichholzer.com>
2017-12-16 14:36:51 +0100

    laden aus DB

--- js/ctrl/openTaskTrackerctrl.js
+++ js/ctrl/openTaskTrackerctrl.js
@@ -26,14 +26,14 @@ openTaskTracker_App.controller('openTaskTracker_Ctrl', function ($scope, $http, 
 
 
 
-    for (var i = 1; i <= 2; ++i) {
+/*    for (var i = 1; i <= 2; ++i) {
         $scope.models.lists.Archiv.push({label: "Projekte " + i +  $scope.beareiter});
-    }
+    }*/
 
 
-    for (var i = 1; i <= 10; ++i) {
+/*    for (var i = 1; i <= 10; ++i) {
         $scope.models.lists.Sprints.push({label: "Sprints A" + i});
-    }
+    }*/
 
 
 
@@ -148,14 +148,64 @@ openTaskTracker_App.controller('openTaskTracker_Ctrl', function ($scope, $http, 
             .success(function (response, status, headers, config) {
                //alert(response.tasks[1].task_name);
                 $scope.tasks = response.tasks;
-                 for( var i = 0; i < response.tasks.length; ++i){
-                    $scope.models.lists.Produktebacklogs.push({label: "Produktbacklogs " + $scope.tasks[i].task_name, listId:"task"+$scope.tasks[i].task_id} );
-                }
+                 for( var i = 0; i < $scope.tasks.length; ++i) {
+                     if (!$scope.tasks[i].task_tst_id != 3) {
+                         if (!$scope.tasks[i].task_spr_id) {
+                             $scope.models.lists.Produktebacklogs.push({
+                                 label: "Produktbacklogs " + $scope.tasks[i].task_name,
+                                 listId: "task" + $scope.tasks[i].task_id
+                             });
+                         }
+                         else if ($scope.tasks[i].task_spr_id) {
+                             $scope.models.lists.Sprints.push({
+                                 label: "Sprint " + $scope.tasks[i].task_name,
+                                 listId: "task" + $scope.tasks[i].task_id
+                             });
+                         }
+                     }
+                     if($scope.tasks[i].task_tst_id == 3) {
+                         $scope.models.lists.Archiv.push({label: "Archiv " + $scope.tasks[i].task_name, listId: "task" + $scope.tasks[i].task_id
+                         });
+
+                     }
+
+                 }
             })
             .error(function (data, status, header, config) {
                 //alert(data);
             });
     }
+
+  /*  $scope.getTasksInSprint =function() {
+        flt.type ="allTasks";
+        // use $.param jQuery function to serialize data from JSON
+        var data = $.param({
+            target_id: "ProjectCollection", //target_id: "WebSwitchCollection"
+            filter_str: flt
+        });
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+
+        $http.post('ajax/getjsondata.php', data, config)
+            .success(function (response, status, headers, config) {
+                //alert(response.tasks[1].task_name);
+                $scope.tasks = response.tasks;
+                for( var i = 0; i < $scope.tasks.length; ++i) {
+                    if ($scope.tasks[i].task_spr_id) {
+                        $scope.models.lists.Sprints.push({label: "Sprint " + $scope.tasks[i].task_name, listId: "task" + $scope.tasks[i].task_id
+                        });
+                    }
+                }
+            })
+            .error(function (data, status, header, config) {
+                //alert(data);
+            });
+
+    }*/
 
     $scope.getGroup =function() {
         flt.type ="group";
